1c4f497b5d7615cda605c32df7f9d6de6c791684
Gaetan de Villele <gdevillele@gmail.com>
2016-11-02 12:00:55 -0700

    add new parsing function that returns the rest of the content
    Signed-off-by: Gaetan de Villele <gdevillele@gmail.com>

--- frontparser.go
+++ frontparser.go
@@ -53,3 +53,39 @@ func ParseFrontmatter(input []byte, out interface{}) error {
 	// parse frontmatter to validate it is valid YAML
 	return yaml.Unmarshal(elements[0], out)
 }
+
+// input is frontmatter + markdown
+// return values
+// - frontmatter
+// - markdown
+// - error
+func ParseFrontmatterAndContent(input []byte) (map[string]interface{}, []byte, error) {
+	var resultFm map[string]interface{} = make(map[string]interface{})
+	var resultRest []byte = make([]byte, 0)
+
+	// remove heading and trailing spaces (and CR, LF, ...)
+	input = bytes.TrimSpace(input)
+	// test for frontmatter delimiter
+	if !bytes.HasPrefix(input, FmYAMLDelimiter) {
+		return resultFm, resultRest, errors.New("heading frontmatter delimiter not found")
+	}
+
+	// trim heading frontmatter delimiter
+	input = bytes.TrimPrefix(input, FmYAMLDelimiter)
+
+	// split on frontmatter delimiter to separate frontmatter from the rest
+	elements := bytes.SplitN(input, FmYAMLDelimiter, 2)
+	if len(elements) != 2 {
+		// malformed input
+		return resultFm, resultRest, errors.New("more than two frontmatter delimiters were found")
+	}
+
+	err := yaml.Unmarshal(elements[0], resultFm)
+	if err != nil {
+		return resultFm, resultRest, err
+	}
+
+	resultRest = elements[1]
+
+	return resultFm, resultRest, nil
+}
