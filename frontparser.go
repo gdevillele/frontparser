297632f47350da63c996f2ce2b2532d8fcebe7c5
Gaetan de Villele <gdevillele@gmail.com>
2016-11-09 17:28:52 -0800

    add SplitFrontmatterAndContent function
    Signed-off-by: Gaetan de Villele <gdevillele@gmail.com>

--- frontparser.go
+++ frontparser.go
@@ -96,3 +96,32 @@ func ParseFrontmatterAndContent(input []byte) (map[string]interface{}, []byte, e
 
 	return resultFm, resultRest, nil
 }
+
+//
+func SplitFrontmatterAndContent(input []byte) ([]byte, []byte, error) {
+
+	resultFm := make([]byte, 0)
+	resultRest := make([]byte, 0)
+
+	// remove heading and trailing spaces (and CR, LF, ...)
+	input = bytes.TrimSpace(input)
+	// test for frontmatter delimiter
+	if !bytes.HasPrefix(input, FmYAMLDelimiter) {
+		return resultFm, resultRest, errors.New("heading frontmatter delimiter not found")
+	}
+
+	// trim heading frontmatter delimiter
+	input = bytes.TrimPrefix(input, FmYAMLDelimiter)
+
+	// split on frontmatter delimiter to separate frontmatter from the rest
+	elements := bytes.SplitN(input, FmYAMLDelimiter, 2)
+	if len(elements) != 2 {
+		// malformed input
+		return resultFm, resultRest, errors.New("more than two frontmatter delimiters were found")
+	}
+
+	resultFm = elements[0]
+	resultRest = elements[1]
+
+	return resultFm, resultRest, nil
+}
